11b233127d928ed0ed9ad4a5c42061b524b38e17
Md Sameem <mohammadsameem559@gmail.com>
2026-04-18 19:51:57 +0530

    adding backend routes

--- frontend/src/links.js
+++ frontend/src/links.js
@@ -1,22 +1,22 @@
-export const loginLink = 'http://localhost:3000/api/auth/login';
-export const signuplink = 'http://localhost:3000/api/auth/register';
-export const getMeLink = 'http://localhost:3000/api/auth/get-me';
-export const logoutLink = 'http://localhost:3000/api/auth/logout';
+export const loginLink = 'https://the-flow-2qgb.onrender.com/api/auth/login';
+export const signuplink = 'https://the-flow-2qgb.onrender.com/api/auth/register';
+export const getMeLink = 'https://the-flow-2qgb.onrender.com/api/auth/get-me';
+export const logoutLink = 'https://the-flow-2qgb.onrender.com/api/auth/logout';
 
-export const getInterviewReportLink = 'http://localhost:3000/api/interview';
-export const downloadResumeLink = 'http://localhost:3000/api/interview/pdf';
-export const resumeScoreLink = 'http://localhost:3000/api/interview/report';
-export const allResumeReportsLink = 'http://localhost:3000/api/interview/all-reports';
+export const getInterviewReportLink = 'https://the-flow-2qgb.onrender.com/api/interview';
+export const downloadResumeLink = 'https://the-flow-2qgb.onrender.com/api/interview/pdf';
+export const resumeScoreLink = 'https://the-flow-2qgb.onrender.com/api/interview/report';
+export const allResumeReportsLink = 'https://the-flow-2qgb.onrender.com/api/interview/all-reports';
 
-export const getPlanLink = 'http://localhost:3000/api/study/plan';
-export const getAllPlansLink = 'http://localhost:3000/api/study/plans';
-export const createPlanLink = 'http://localhost:3000/api/study/plan';
+export const getPlanLink = 'https://the-flow-2qgb.onrender.com/api/study/plan';
+export const getAllPlansLink = 'https://the-flow-2qgb.onrender.com/api/study/plans';
+export const createPlanLink = 'https://the-flow-2qgb.onrender.com/api/study/plan';
 
-export const fetchAllTasksLink = 'http://localhost:3000/api/tasks';
-export const deleteTaskLinkPrefix = 'http://localhost:3000/api/tasks';
-export const addTasksLink = 'http://localhost:3000/api/tasks';
-export const updateTaskLinkPrefix = 'http://localhost:3000/api/tasks/status';
-export const updateTaskTitleLink = 'http://localhost:3000/api/tasks';
-export const updateTaskDeadlineLink = 'http://localhost:3000/api/tasks/deadline';
+export const fetchAllTasksLink = 'https://the-flow-2qgb.onrender.com/api/tasks';
+export const deleteTaskLinkPrefix = 'https://the-flow-2qgb.onrender.com/api/tasks';
+export const addTasksLink = 'https://the-flow-2qgb.onrender.com/api/tasks';
+export const updateTaskLinkPrefix = 'https://the-flow-2qgb.onrender.com/api/tasks/status';
+export const updateTaskTitleLink = 'https://the-flow-2qgb.onrender.com/api/tasks';
+export const updateTaskDeadlineLink = 'https://the-flow-2qgb.onrender.com/api/tasks/deadline';
 
-export const allRoadmapsLink = 'http://localhost:3000/api/interview/roadmap/all';
+export const allRoadmapsLink = 'https://the-flow-2qgb.onrender.com/api/interview/roadmap/all';
